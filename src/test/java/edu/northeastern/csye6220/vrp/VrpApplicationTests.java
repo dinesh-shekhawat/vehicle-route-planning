77b752ec7624996c573871dd4b341542d454fe41
Dinesh Shekhawat <shekhawat.d@northeastern.edu>
2023-11-26 21:38:55 -0500

    Initiliazed spring boot application

---
+++ src/test/java/edu/northeastern/csye6220/vrp/VrpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.northeastern.csye6220.vrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
