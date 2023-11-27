3c8da27102df1104780d2f459cbdddc8360d95c9
Dinesh Shekhawat <shekhawat.d@northeastern.edu>
2023-11-26 22:11:38 -0500

    Configured logging

--- src/main/java/edu/northeastern/csye6220/vehicleRoutePlanning/VrpApplication.java
+++ src/main/java/edu/northeastern/csye6220/vehicleRoutePlanning/VrpApplication.java
@@ -1,12 +1,17 @@
 package edu.northeastern.csye6220.vehicleRoutePlanning;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VrpApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(VrpApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("Starting the Vehicle Route Planning Application");
 		SpringApplication.run(VrpApplication.class, args);
 	}
 
--- src/main/java/edu/northeastern/csye6220/vehicleRoutePlanning/controller/HomeController.java
+++ src/main/java/edu/northeastern/csye6220/vehicleRoutePlanning/controller/HomeController.java
@@ -1,13 +1,18 @@
 package edu.northeastern.csye6220.vehicleRoutePlanning.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	
 	@GetMapping("/")
 	public String home() {
+		LOGGER.trace("accessing get method");
 		return "index";
 	}
 	
