8ee73cf99d9c8d1466ac179a176a0b17118057e1
Dinesh Shekhawat <shekhawat.d@northeastern.edu>
2024-01-06 15:23:18 -0500

    Add script for remote debugging

---
+++ debug-server-start.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# Default values
+LOG_CONF_FILE=config-log-files/log4j2.xml
+
+#HIBERNATE_HBM2DDL_AUTO=update
+HIBERNATE_HBM2DDL_AUTO=validate
+
+# Remote Debugging Port
+DEBUG_PORT=5005
+
+# Display variable values
+echo "LOG_CONF_FILE: $LOG_CONF_FILE"
+echo "HIBERNATE_HBM2DDL_AUTO: $HIBERNATE_HBM2DDL_AUTO"
+echo "DEBUG_PORT: $DEBUG_PORT"
+
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=0.0.0.0:$DEBUG_PORT -Dlogging.config=$LOG_CONF_FILE -Dhibernate.hbm2ddlAuto=$HIBERNATE_HBM2DDL_AUTO"
